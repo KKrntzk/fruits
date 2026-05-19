c22d83f09ee23367846da243a931ad961b57fa15
KKrntzk <sophie.kornetzki@gmail.com>
2026-05-19 13:47:10 +0200

    dummy_data added

---
+++ fruit_app/dummy_data.py
@@ -0,0 +1,7 @@
+FRUITS_LIST = [
+    {"name": "Apfel", "gewicht": 150, "farbe": "Rot"},
+    {"name": "Banane", "gewicht": 120, "farbe": "Gelb"},
+    {"name": "Blaubeere", "gewicht": 2, "farbe": "Blau"},
+    {"name": "Erdbeere", "gewicht": 15, "farbe": "Rot"},
+    {"name": "Melone", "gewicht": 4000, "farbe": "Grün"}
+]
