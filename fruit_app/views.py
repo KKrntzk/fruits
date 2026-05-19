3d0ca2ad8f8732548777c7c8700f167c8e9240b2
KKrntzk <sophie.kornetzki@gmail.com>
2026-05-19 14:03:36 +0200

    views and urls py added

--- fruit_app/views.py
+++ fruit_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
+from .dummy_data import FRUITS_LIST
 # Create your views here.
+
+def send_fruits(request):
+    data = {
+        'fruits': FRUITS_LIST
+    }
+    return JsonResponse(data)
